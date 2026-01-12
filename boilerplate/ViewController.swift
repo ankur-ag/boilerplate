db69ed2a5f7ce2a6c983340c9b485acb35c62ffe
Ankur Agarwal <outlaws01@gmail.com>
2026-01-12 10:08:16 +0800

    Initial Commit

---
+++ boilerplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  boilerplate
+//
+//  Created by Ankur on 1/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ boilerplateTests/boilerplateTests.swift
@@ -0,0 +1,17 @@
+//
+//  boilerplateTests.swift
+//  boilerplateTests
+//
+//  Created by Ankur on 1/12/26.
+//
+
+import Testing
+@testable import boilerplate
+
+struct boilerplateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
